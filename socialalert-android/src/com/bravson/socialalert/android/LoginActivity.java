af599a229dc7cc691e1f56d77e6a01f6d670b6ae
Lucien <lgarin@gmx.ch>
2015-11-12 19:42:31 +0100

    sample with annotation processing and maven

---
+++ socialalert-android/src/com/bravson/socialalert/android/LoginActivity.java
@@ -0,0 +1,10 @@
+package com.bravson.socialalert.android;
+
+import org.androidannotations.annotations.EActivity;
+
+import android.app.Activity;
+
+@EActivity(R.layout.login)
+public class LoginActivity extends Activity {
+
+}
